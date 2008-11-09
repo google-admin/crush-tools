e0b22f470f224317cbebbe40665bff814b6693b8
jeremy.hinds <jeremy.hinds@9f305d85-0c50-0410-b3e0-65f4174694de>
2008-11-09 04:48:32 +0000

    Add a field_str() function to perllib/utils.pl, equivalent to the C function of the same name in libcrush.

--- branches/labels_as_field_specifiers/src/perllib/utils.pl
+++ branches/labels_as_field_specifiers/src/perllib/utils.pl
@@ -8,4 +8,24 @@ sub expand_chars {
   eval("sprintf(\"$d\")");
 }
 
+=item * field_str()
+
+returns the 0-based index of the first field in a delimited string equal to
+the specified value, or undef if not found.
+
+=cut
+sub field_str {
+  my $value = shift;
+  my $string = shift;
+  my $delim = shift;
+  $string =~ s/[\r\n]//g;
+  my @a = split(/\Q$delim\E/, $string);
+  my $i;
+  for $i (0 .. $#a) {
+    if ($a[$i] eq $value) {
+      return $i;
+    }
+  }
+  return undef;
+}
 1;
